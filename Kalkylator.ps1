9557225469da84049feb5da953b331e4c45720ee
Serex96 <christian.strandbrink@live.se>
2014-11-27 13:59:45 +0100

    Poweshell

---
+++ Kalkylator.ps1
@@ -0,0 +1,34 @@
+﻿$raknesatt = read-host "Skriv in ett räknesätt enligt siffrorna = 
+Addition(1)
+Division(2)
+Multiplikation(3)
+Subtraktion(4)"
+[int]$heltal1 = read-host "Skriv in ett tal"
+[int]$heltal2 = read-host "Skriv in ett till tal"
+
+if($raknesatt -eq 1) 
+    {
+    $funktion = "+"
+    $resultat = $heltal1 + $heltal2
+    }
+elseif($raknesatt -eq 2) 
+    {
+    $funktion = "/"
+    $resultat = $heltal1 / $heltal2
+    }
+elseif($raknesatt -eq 3) 
+    {
+    $funktion = "*"
+    $resultat = $heltal1 * $heltal2
+    }
+elseif($raknesatt -eq 4)
+    {
+    $funktion = "-"
+    $resultat = $heltal1 - $heltal2
+    }
+else{
+write-host "ERROR"
+return
+} 
+
+write-host $heltal1 $funktion $heltal2 = $resultat 
---
+++ Snake eye.ps1
@@ -0,0 +1,24 @@
+﻿# Den här uppgiften kallas Snake Eyes och handlar om att beräkna sannolikhet med hjälp av Powershells matematik.#
+
+
+
+
+$antalsnake = 0
+$antalslag = Read-host "Hur många gånger vill du kasta två tärning?"
+for ($i = 0; $i -le $antalslag; $i ++) { 
+    $tal1=get-random -minimum 1 -Maximum 7
+    $tal2=get-random -minimum 1 -Maximum 7
+
+
+    if ($tal1 -eq 1 -and $tal2 -eq 1) {
+
+
+        $antalsnake++
+    }
+}
+    $decimal = $antalsnake/$i
+    $procent = $decimal * 100
+Write-host "Antalet gånger du valde att kasta tärningarna = " $antalslag
+Write-host "Antal gånger det blev 1 1 =" $antalsnake
+Write-host "Antal procent av gångerna det blev 1 1 = "$procent "%"
+
