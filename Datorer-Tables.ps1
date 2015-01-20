df886590f8a21f09a7ef7225ac1ae877ea3e7520
Serex96 <christian.strandbrink@live.se>
2015-01-20 20:17:24 +0100

    Hash Tables

---
+++ Datorer-Tables.ps1
@@ -0,0 +1,22 @@
+﻿$datorer = Get-Content C:\Powershell\datorer.txt
+$hashtables=@{}
+for($i=0; $i -lt $datorer.Count-50)
+
+
+{
+       
+      $table = $datorer[$i].Split(";")
+
+      $hashtables.add($table[0],$table[2])
+      $i++
+$a = @{Expression={$table[1]};Label="Host namn";width=30}, `
+@{Expression={$table[0]};Label="IP-adresser";width=30}, `
+@{Expression={$table[2]};Label="Mac-adresser";width=20}
+}
+ 
+
+
+ 
+
+
+$a | Format-Table $a  
---
+++ Datorer-Tablesda.ps1
@@ -0,0 +1,21 @@
+﻿$datorer = Get-Content C:\Powershell\datorer.txt
+$hashtables=@{}
+for($i=0; $i -lt $datorer.Count)
+
+
+{
+       
+      $table = $datorer[$i].Split(";")
+      $apa = $table[1]+";"+$table[2]
+
+      $hashtables.add($table[0], $apa)
+      $i++
+
+}
+
+
+
+$hashtables.Values]
+
+
+   
